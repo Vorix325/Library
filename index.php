ca7d5d0adf4c971c2e72a4b1da2dd0d0b27c3e7d
Vorix325 <fftpnhan2246@gmail.com>
2022-12-03 12:38:27 -0800

    create some index

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
-<?php
-
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+<?php include("view/header.php"); ?>
+<main>
+    <h1>Library Main Page</h1>
+</main>
+<?php include("view/footer.php"); ?>
---
+++ view/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+     <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap"
+            rel="stylesheet"
+        />
+        <!-- Stylesheet -->
+        <link rel="stylesheet" href="../bp-stylesheet.css" >
+</head>   
+<body>
+         <div class="header">
+            <div class="inner-header">
+                <div class="logo-container">
+                    <h1>Budget<span>Buddy</span></h1>
+                </div>
+                <ul class="navigation">
+                    <a href='/ex_starts/BudgetBuddy/BudgetBuddy/index.php'><li>Home</li></a>
+                    <a href= '/ex_starts/BudgetBuddy/BudgetBuddy/user_info/index.php?action=show_login'><li>Login</li></a>
+                    <a href= '/ex_starts/BudgetBuddy/BudgetBuddy/user_info/index.php?action=show_reg'><li>Register</li></a>
+                    <a href='/ex_starts/BudgetBuddy/BudgetBuddy/user_info/index.php?action=show_profile'><li>Profile</li></a>
+                     
+                
+                </ul>
+            </div>
+         </div>
+
